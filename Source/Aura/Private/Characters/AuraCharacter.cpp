80f44a1119569f18595c528d62ac1faa790e3b8a
CodingCocho <raulujr@gmail.com>
2023-12-16 12:00:43 -0800

    Health and Mana

--- Source/Aura/Private/Characters/AuraCharacter.cpp
+++ Source/Aura/Private/Characters/AuraCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "AbilitySystemComponent.h"
 #include "Characters/AuraCharacter.h"
+#include "AbilitySystemComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Player/AuraPlayerState.h"
 
